f71041bced8db6765709629b9ae00e87b22c4487
Musa Alabdulhadi <houdq8@gmail.com>
2020-06-27 21:39:58 +0300

    Done! thank you nancy!

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,34 +1,48 @@
 // السؤال الأول
-// 1
-
+// 1 
+const name = "اسمي موسى وعمري "
 // 2
-
+const age = 17
 // 3 and 4
-
+console.log(name + age)
 // 5 and 6
-
+console.log( age*5)
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobbies = ["الصيد" + " " + "السباحه" + " " + "التسلق" + " "];
 // 2
+hobbies.push = (16);
+
+hobbies[4] = 16
+console.log(hobbies);
+console.log(hobbies.length);
 
 // 3
-
+hobbies.pop()
+console.log(hobbies)
 // 4
-
+    let student = {
+    path: "web programming",
+    language: "CSS",
+    }
 // 5
+student ["TAs"] = ["Nancy", "Moudhi", "Hussain", "Rahaf"];
 
+console.log(student)
 // 6
-
+console.log(student.path)
 // 7
-
+console.log(student["TAs"][0])
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
-
+ 
 // البونص
 // 1
-
+student["sayHello"] = function() {console.log("مرحباً")}
+console.log(student)
 // 2
-
+student["sayHello"]()
+student["sayHello"]()
+student["sayHello"]()
 // console results => console.png
